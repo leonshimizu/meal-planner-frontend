aa6837b3ba986071c08abea24c0e448f11e41507
“leonshimizu” <lmshimizu@gmail.com>
2021-11-28 10:33:01 -0800

    started deploying to netlify, still need to fix apikeys and link from front to backend

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://your-backend-app-name.herokuapp.com";
 
 new Vue({
   router,
