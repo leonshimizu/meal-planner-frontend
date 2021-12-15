e68a8ae57ff9509d21007a56100c920d3cc6f45f
“leonshimizu” <lmshimizu@gmail.com>
2021-12-14 18:45:52 -0800

    started editing personal meal plan page - still need to add show modal and add the day of the week

--- src/views/MealPlan.vue
+++ src/views/MealPlan.vue
@@ -1,13 +1,49 @@
 <template>
+  <!-- Header-->
   <div class="mealPlan">
-    <h1>{{ message }}</h1>
-    <p>Diet: {{ meal_plan[0].diet }}</p>
-    <p>Allergies: {{ meal_plan[0].allergies }}</p>
-    <p>Time Frame: {{ meal_plan[0].timeFrame }}</p>
-    <p>Calories: {{ meal_plan[0].calories }}</p>
-    <p>Created: {{ meal_plan[0].created_at }}</p>
-    <hr>
-    <!-- {{ mealData }} -->
+    <header class="bg-dark py-5">
+      <div class="container px-4 px-lg-5 my-5">
+          <div class="text-center text-white">
+              <h1 class="display-4 fw-bolder">{{ message }}</h1>
+              <p class="lead fw-normal text-white-50 mb-0">Diet: {{ meal_plan[0].diet }}</p>
+              <p class="lead fw-normal text-white-50 mb-0">Allergies: {{ meal_plan[0].allergies }}</p>
+              <p class="lead fw-normal text-white-50 mb-0">Time Frame: {{ meal_plan[0].timeFrame }}</p>
+              <p class="lead fw-normal text-white-50 mb-0">Calories: {{ meal_plan[0].calories }}</p>
+              <p class="lead fw-normal text-white-50 mb-0">Created: {{ meal_plan[0].created_at }}</p>
+          </div>
+      </div>
+    </header>
+
+    <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-3 justify-content-center">
+                <div class="col mb-5" v-for="meal in allMeals">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img v-bind:src="meal.image" class="card-img-top" alt="..." style="max-width: 500px; text-align:center">
+                        <!-- Product details-->
+                        <div class="card-body p-3">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">Title: {{ meal.title }}</h5>
+                                <!-- Product price-->
+                                <p>Prep Time: {{ meal.preparationMinutes }}</p>
+                                <p>Cook Time: {{ meal.cookingMinutes }}</p>
+                                <p>Servings: {{ meal.servings }}</p>
+                                <p>Price Per Serving: {{ meal.pricePerServing }}</p>
+                                <!-- <button type="button" class="btn btn-primary" v-on:click="showRecipeInfo(value)">Show More Info</button> -->
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>  
+<!-- 
     <ul>
       <li v-for="meal in allMeals">
         <p><strong>Title: {{ meal.title }}</strong></p>
@@ -33,12 +69,12 @@
           {{ info.nutrition && info.nutrition.nutrients[5].title }}: {{ info.nutrition && info.nutrition.nutrients[5].amount }} {{ info.nutrition && info.nutrition.nutrients[5].unit }}
           {{ info.nutrition && info.nutrition.nutrients[6].title }}: {{ info.nutrition && info.nutrition.nutrients[6].amount }} {{ info.nutrition && info.nutrition.nutrients[6].unit }}
           {{ info.nutrition && info.nutrition.nutrients[7].title }}: {{ info.nutrition && info.nutrition.nutrients[7].amount }} {{ info.nutrition && info.nutrition.nutrients[7].unit }}
-          <!-- {{ info.nutrition && info.nutrition.nutrients[8].title }}: {{ info.nutrition && info.nutrition.nutrients[8].amount }} {{ info.nutrition && info.nutrition.nutrients[8].unit }} -->
-          <!-- {{ info.nutrition && info.nutrition.nutrients[9].title }}: {{ info.nutrition && info.nutrition.nutrients[9].amount }} {{ info.nutrition && info.nutrition.nutrients[9].unit }} -->
+          {{ info.nutrition && info.nutrition.nutrients[8].title }}: {{ info.nutrition && info.nutrition.nutrients[8].amount }} {{ info.nutrition && info.nutrition.nutrients[8].unit }}
+          {{ info.nutrition && info.nutrition.nutrients[9].title }}: {{ info.nutrition && info.nutrition.nutrients[9].amount }} {{ info.nutrition && info.nutrition.nutrients[9].unit }}
         </p>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog> -->
   </div>
 </template>
 
