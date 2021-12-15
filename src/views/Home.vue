2311c5e215b8d59a5d061e14f49195eeb0c33fb9
“leonshimizu” <lmshimizu@gmail.com>
2021-12-14 17:01:14 -0800

    started adding cards for each day of the week - need to add photos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,61 @@
 <template>
   <div class="home">
-          <!-- Header-->
-      <header class="bg-dark py-5">
-          <div class="container px-4 px-lg-5 my-5">
-              <div class="text-center text-white">
-                  <h1 class="display-4 fw-bolder">Welcome to the Meal Plan Generator!</h1>
-                  <p class="lead fw-normal text-white-50 mb-0">Please select from the following diets:</p>
-                  <p class="fw-normal text-white-50 mb-0">Diets: Gluten Free, Ketogenic, Vegetarian, Lacto-Vegetarian, Ovo-Vegetarian, Vegan, Pescetarian, Paleo, Primal, low FODMAP, or Whole30 (for more infomation, visit <a href="https://spoonacular.com/food-api/docs#Diets">Diets)</a></p>
-                  <p class="fw-normal text-white-50 mb-0">Please, input your filters of choice:</p>
-                  <br>
-                  <div class="input-group input-group-sm mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Diet</span>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="diet">
-                  </div>
-                  <div class="input-group input-group-sm mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Calories</span>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="calories">
-                  </div>
-                  <div class="input-group input-group-sm mb-3">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Allergies</span>
-                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="allergies">
-                  </div>
-                  <button type="button" class="btn btn-success" v-on:click="createMealPlan()">Generate Meal Plan</button>
-                  <br>
-                  <br>
-                  <button type="button" class="btn btn-success" v-on:click="saveMealPlan()">Save Meal Plan</button>
-              </div>
-          </div>
-      </header>
-    <!-- <h1>Welcome to the Meal Plan Generator!</h1>
-    <h3>Please select from the following diets:</h3>
-    <p>Diets: Gluten Free, Ketogenic, Vegetarian, Lacto-Vegetarian, Ovo-Vegetarian, Vegan, Pescetarian, Paleo, Primal, low FODMAP, or Whole30 (for more infomation, visit <a href="https://spoonacular.com/food-api/docs#Diets">Diets)</a></p>
-    <p>Now please, input your filters of choice:</p>
-    <input type="text" v-model="diet">
-    <input type="text" v-model="calories">
-    <input type="text" v-model="allergies">
-    <br>
-    <button v-on:click="createMealPlan()">Generate Meal Plan</button>
-    <br>
-    <button v-on:click="saveMealPlan()">Save Meal Plan</button>
-    <br> -->
+    <!-- Header-->
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">Welcome to the Meal Plan Generator!</h1>
+                <p class="lead fw-normal text-white-50 mb-0">Please select from the following diets:</p>
+                <p class="fw-normal text-white-50 mb-0">Diets: Gluten Free, Ketogenic, Vegetarian, Lacto-Vegetarian, Ovo-Vegetarian, Vegan, Pescetarian, Paleo, Primal, low FODMAP, or Whole30 (for more infomation, visit <a href="https://spoonacular.com/food-api/docs#Diets">Diets)</a></p>
+                <p class="fw-normal text-white-50 mb-0">Please, input your filters of choice:</p>
+                <br>
+                <div class="input-group input-group-sm mb-3">
+                  <span class="input-group-text" id="inputGroup-sizing-sm">Diet</span>
+                  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="diet">
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                  <span class="input-group-text" id="inputGroup-sizing-sm">Calories</span>
+                  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="calories">
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                  <span class="input-group-text" id="inputGroup-sizing-sm">Allergies</span>
+                  <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" v-model="allergies">
+                </div>
+                <button type="button" class="btn btn-success" v-on:click="createMealPlan()">Generate Meal Plan</button>
+                <br>
+                <br>
+                <button type="button" class="btn btn-success" v-on:click="saveMealPlan()">Save Meal Plan</button>
+            </div>
+        </div>
+    </header>
+    <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                <div class="col mb-5" v-for="(value, key) in days">
+                    <div class="card h-100">
+                        <!-- Product image-->
+                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                        <!-- Product details-->
+                        <div class="card-body p-4">
+                            <div class="text-center">
+                                <!-- Product name-->
+                                <h5 class="fw-bolder">{{ key.charAt(0).toUpperCase() + key.slice(1) }}</h5>
+                                <!-- Product price-->
+                                <p>Breakfast: {{ value.meals[0].title }}</p>
+                                <p>Lunch: {{ value.meals[1].title }}</p>
+                                <p>Dinner: {{ value.meals[2].title }}</p>
+                                <button type="button" class="btn btn-primary" v-on:click="showRecipeInfo(value)">Show More Info</button>
+                            </div>
+                        </div>
+                        <!-- Product actions-->
+                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>      
     <ul>
       <li v-for="(value, key) in days">
         <p><b>{{ key.charAt(0).toUpperCase() + key.slice(1) }}</b></p>
