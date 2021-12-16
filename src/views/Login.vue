74242e6f6abd0515032252c2aa97b86d0341fa08
“leonshimizu” <lmshimizu@gmail.com>
2021-12-16 13:54:47 -0800

    added header to signup and login pages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="login">
-    <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newSessionParams.email" />
+    <header class="bg-dark py-5">
+      <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+          <form v-on:submit.prevent="submit()">
+            <h1 class="display-4 fw-bolder">Login</h1>
+            <ul>
+              <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+            </ul>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Email:</label>
+              <input type="email" v-model="newSessionParams.email" />
+            </div>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Password:</label>
+              <input type="password" v-model="newSessionParams.password" />
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newSessionParams.password" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+    </header>
   </div>
 </template>
 
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="signup">
-    <form v-on:submit.prevent="submit()">
-      <h1>Signup</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Name:</label>
-        <input type="text" v-model="newUserParams.name" /> <small v-if="newUserParams.name.length > 0 && newUserParams.name.length <= 20">{{ newUserParams.name.length }}</small> <small v-if="newUserParams.name.length > 20" class="text-danger">Name is too long</small>
+    <header class="bg-dark py-5">
+      <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+          <form v-on:submit.prevent="submit()">
+            <h1 class="display-4 fw-bolder">Signup</h1>
+            <ul>
+              <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+            </ul>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Name:</label>
+              <input type="text" v-model="newUserParams.name" /> <small v-if="newUserParams.name.length > 0 && newUserParams.name.length <= 20">{{ newUserParams.name.length }}</small> <small v-if="newUserParams.name.length > 20" class="text-danger">Name is too long</small>
+            </div>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Email:</label>
+              <input type="email" v-model="newUserParams.email" />
+            </div>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Password:</label>
+              <input type="password" v-model="newUserParams.password" />
+            </div>
+            <div>
+              <label class="lead fw-normal text-white-50 mb-0">Password confirmation:</label>
+              <input type="password" v-model="newUserParams.password_confirmation" /><small v-if="newUserParams.password !== newUserParams.password_confirmation" class="text-danger">Passwords must match</small>
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newUserParams.email" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newUserParams.password" />
-      </div>
-      <div>
-        <label>Password confirmation:</label>
-        <input type="password" v-model="newUserParams.password_confirmation" /><small v-if="newUserParams.password !== newUserParams.password_confirmation" class="text-danger">Passwords must match</small>
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
+    </header>
   </div>
 </template>
 
