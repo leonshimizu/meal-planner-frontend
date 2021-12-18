f99def012bc058978980919aa4a68b6e9a4f6ef9
“leonshimizu” <lmshimizu@gmail.com>
2021-12-17 20:44:28 -0800

    removed list dot from authentication

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@
           <a class="navbar-brand" href="/">Hafa Adai!</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                  <li class="nav-item"><a class="nav-link active" aria-current="page" href="/meal_plans">Personal Meal Plan Page</a></li>
-                  <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-              </ul>
-              <li class="nav-item dropdown">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="/meal_plans">Personal Meal Plan Page</a></li>
+                <li class="nav-item"><a class="nav-link" href="/about">About</a></li>
+            </ul>
+            <span class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Authentication</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="/signup">Signup</a></li>
@@ -18,7 +18,7 @@
                     <li><a class="dropdown-item" href="/login">Login</a></li>
                     <li><a class="dropdown-item" href="/logout">Logout</a></li>
                 </ul>
-            </li>
+            </span>
           </div>
       </div>
     </nav>
