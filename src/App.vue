cba0e274b4b1eb0f8e06a7bf1342de2b50ec1f24
“leonshimizu” <lmshimizu@gmail.com>
2021-12-17 19:48:01 -0800

    stopped the home page from running the meal plan generate action at page load - runs only after clicking the button

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
 
     <!-- Footer-->
     <footer class="py-5 bg-dark">
-        <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        <div class="container"><p class="m-0 text-center text-white">Thank you! :) </p></div>
     </footer>    
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -175,7 +175,7 @@ export default {
     }
   },
   created: function() {
-    this.createMealPlan();
+    // this.createMealPlan();
   },
   methods: {
     createMealPlan: function() {
@@ -226,26 +226,3 @@ export default {
   }
 }
 </script>
-
-<!-- CAROUSEL - doesn't loop properly
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://thestayathomechef.com/wp-content/uploads/2019/01/Pot-Roast-1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://www.jocooks.com/wp-content/uploads/2019/10/coconut-chicken-curry-1-10.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://therecipecritic.com/wp-content/uploads/2021/07/streettacos-500x500.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div> -->
