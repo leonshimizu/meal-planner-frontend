ed3dd0121fd59c20792c52d2d1a7e4e49011ed44
“leonshimizu” <lmshimizu@gmail.com>
2021-12-20 23:07:16 -0800

    added style to recipe page to make it the same as the home page and made cards slightly larger

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,7 +36,7 @@
     <!-- Section -->
     <LoadingScreen v-if="isLoading"></LoadingScreen>
     <section class="py-5">
-        <div class="container px-4 px-lg-5 mt-5">
+        <div class="container px-4 px-lg-10 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <div class="col mb-5" v-for="(value, key, $index) in days" v-if="$index < 7">
                     <div class="card h-100 border-dark mb-3">
--- src/views/MealPlan.vue
+++ src/views/MealPlan.vue
@@ -2,7 +2,7 @@
   <!-- Header -->
   <div class="mealPlan">
     <header class="bg-dark py-5" v-if="user !== null">
-      <div class="container px-4 px-lg-5 my-5">
+      <div class="container px-4 px-lg-10 my-5">
           <div class="text-center text-white">
               <h1 class="display-4 fw-bolder">{{ user.name }}'s Meal Plan:</h1>
               <p class="lead fw-normal text-white-50 mb-0">Diet: {{ meal_plan[0].diet }}</p>
